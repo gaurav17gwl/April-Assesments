c75b161e1598f7aae7a406da88e25a18dfa38cc7
gaurav17gwl <130116167+gaurav17gwl@users.noreply.github.com>
2023-04-30 13:41:28 +0530

    PSET-4 Solution uploaded

---
+++ PSET-4/PSET-4.py
@@ -0,0 +1,5 @@
+total_apple = 10
+apple_eaten = 4
+
+#print('There are ' + total_apple - apple_eaten + ' left in the basket.' )
+print('there are ' + str(total_apple-apple_eaten) + ' apples left in the basket.')
